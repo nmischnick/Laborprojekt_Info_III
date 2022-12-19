6dbd0294c9209fdbf1d62b0d435daaccb7e85d9e
jbaloh <jurcek.baloh@gmail.com>
2022-12-19 15:40:04 +0100

    succes in die Files Funktion hinzugefügt

--- Octoprint/json_filter.py
+++ Octoprint/json_filter.py
@@ -7,14 +7,9 @@ import ast
 import json
 import octoprint
 
-
 PRINTER_URL = "http://141.41.42.192/api/printer?apikey=A7DBE849344A42A0B22C50CA22EC6210"
 JOB_URL = "http://141.41.42.192/api/job?apikey=A7DBE849344A42A0B22C50CA22EC6210"
 FILES_URL = "http://141.41.42.192/api/files?apikey=A7DBE849344A42A0B22C50CA22EC6210"
-
-PRINTER_API ='{"sd":{"ready":true},"state":{"error":"","flags":{"cancelling":false,"closedOrError":false,"error":false,"finishing":false,"operational":true,"paused":true,"pausing":false,"printing":false,"ready":false,"resuming":false,"sdReady":true},"text":"Paused"},"temperature":{"W":{"actual":0.0,"offset":0,"target":null},"bed":{"actual":64.94,"offset":0,"target":65.0},"tool0":{"actual":199.67,"offset":0,"target":200.0}}}'
-JOB_API = '{"job":{"averagePrintTime":4988.674538585007,"estimatedPrintTime":3135.1295167632998,"filament":{"tool0":{"length":2991.2771200002394,"volume":7.194864641049301}},"file":{"date":1657719794,"display":"Handyhalter_TQM_1.gcode","name":"Handyhalter_TQM_1.gcode","origin":"local","path":"Handyhalter_TQM_1.gcode","size":1980023},"lastPrintTime":5049.7762020089995,"user":"Triltsch"},"progress":{"completion":0.2779260644952104,"filepos":5503,"printTime":109,"printTimeLeft":4943,"printTimeLeftOrigin":"average"},"state":"Paused"}'
-FILES_API = '{"files":[{"date":1655993823,"display":"Form-Kubus_0.2mm_PETG_MK3S_25m.gcode","gcodeAnalysis":{"dimensions":{"depth":104.754,"height":18.0,"width":101.754},"estimatedPrintTime":1415.1325518024532,"filament":{"tool0":{"length":1013.5587899999985,"volume":2.4378945872441}},"printingArea":{"maxX":101.754,"maxY":101.754,"maxZ":18.0,"minX":0.0,"minY":-3.0,"minZ":0.0}},"hash":"732bc17d3771299b1c86272d75e6fb1522f3bcd5","name":"Form-Kubus_0.2mm_PETG_MK3S_25m.gcode","origin":"local","path":"Form-Kubus_0.2mm_PETG_MK3S_25m.gcode","prints":{"failure":3,"last":{"date":1656241873.8742633,"success":false},"success":0},"refs":{"download":"http://141.41.42.192/downloads/files/local/Form-Kubus_0.2mm_PETG_MK3S_25m.gcode","resource":"http://141.41.42.192/api/files/local/Form-Kubus_0.2mm_PETG_MK3S_25m.gcode"},"size":498343,"statistics":{"averagePrintTime":{},"lastPrintTime":{}},"type":"machinecode","typePath":["machinecode","gcode"]} ],"free":27833102336,"total":31119536128}'
 
 def printer_api_f(json_str):
     "Die Funktion bearbeitet den Json_printer_api_string. Dabei werden die False Werte rausgelöscht."
@@ -51,11 +46,8 @@ def files_api_f(json_str):
     "Die Funktion bearveitet den Json_files_api_string. Da der String aus mehreren Bäumen besteht, werden die nacheinander bearbeitet."
     json_info = json.loads(json_str)    #json wird gelesen
     files = json_info['files']          #json baum wird definiert
-    i = 0
     liste =[]
-    while "date" in files[i]:
-        i = i+1
-    for zaehler in range(0,i):
+    for i in range(0,len(files)):
         file = files[i]                     #der n baum files wird ausgewählt
         try:
             display = file['display']           #daten werden definiert mit path
@@ -73,7 +65,11 @@ def files_api_f(json_str):
             free = json_info['free']
         except:
             free= None
-        files_api_data = {'hash':hash_data, 'display':display,'download':download,'free':free} #daten in eigene dict
+        try:
+            success = json_info['prints']['success']
+        except:
+            success = None
+        files_api_data = {'hash':hash_data, 'display':display,'download':download,'free':free,'success':success} #daten in eigene dict
         liste.append(files_api_data)
     return liste
 
