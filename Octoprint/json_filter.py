5f1aca6a429c9065474c600e7e7bb1f81fceefa5
jbaloh <jurcek.baloh@gmail.com>
2022-12-20 13:01:32 +0100

    date in die job Funktion hinzugefügt + Kommentare

--- Octoprint/json_filter.py
+++ Octoprint/json_filter.py
@@ -1,7 +1,7 @@
 """
 Diese Datei bearbeitet die Json Strings in beötigte Werte
 Autor: Jure Baloh
-Datum: 14.12.2022 15:18
+Datum: 20.12.2022
 """
 import ast
 import json
@@ -20,7 +20,7 @@ def printer_api_f(json_str):
     for key, value in dict(flags).items():
         if value is False:                      # wenn key = false wird entfernt
             del flags[key]
-        else:                                   #wenn key "! false wird er in die dict hinzugefügt
+        else:                                   #wenn key != false wird er in die dict hinzugefügt
             name = key
             value = flags[key]
             state[name] = value
@@ -34,21 +34,22 @@ def printer_api_f(json_str):
     return printer_api_data
 
 def job_api_f(json_str):
-    "Die Funktion bearveitet den Json_job_api_string. Die wichtigen Werte werden im neuen Dict gespeichert."
+    "Die Funktion bearbeitet den Json_job_api_string. Die wichtigen Werte werden im neuen Dict gespeichert."
     json_info = json.loads(json_str)                            #json wird gelesen
     average_print_time =json_info["job"]['averagePrintTime']      #daten definieren mit path
     volume = json_info['job']['filament']['tool0']['volume']
     display = json_info['job']['file']['display']
-    job_api_data = {'averagePrintTime':average_print_time,"volume":volume,"display":display}  #daten in eigene Dict
+    date = json_info['job']['file']['date']
+    job_api_data = {'averagePrintTime':average_print_time,"volume":volume,"date":date,"display":display}  #daten in eigene Dict
     return job_api_data
 
 def files_api_f(json_str):
-    "Die Funktion bearveitet den Json_files_api_string. Da der String aus mehreren Bäumen besteht, werden die nacheinander bearbeitet."
+    "Die Funktion bearbeitet den Json_files_api_string. Da der String aus mehreren Bäumen besteht, werden die nacheinander bearbeitet."
     json_info = json.loads(json_str)    #json wird gelesen
     files = json_info['files']          #json baum wird definiert
     liste =[]
     for i in range(0,len(files)):
-        file = files[i]                     #der n baum files wird ausgewählt
+        file = files[i]                     #der n tree des Files wird ausgewählt
         try:
             display = file['display']           #daten werden definiert mit path
         except:
@@ -73,10 +74,10 @@ def files_api_f(json_str):
         liste.append(files_api_data)
     return liste
 
-printer_api = octoprint.get_json(PRINTER_URL)
-printer_api = ast.literal_eval(printer_api)
-printer_api = json.dumps(printer_api)
-werte_printer_api = printer_api_f(printer_api)
+printer_api = octoprint.get_json(PRINTER_URL)   #json str wird aufgerufen
+printer_api = ast.literal_eval(printer_api)     #wertet die json Funktion mit trees richtig aus
+printer_api = json.dumps(printer_api)           #konvertiert den ausgewerteten Wert in den passenden JSON
+werte_printer_api = printer_api_f(printer_api)  #ruft die Funktion auf
 
 job_api = octoprint.get_json(JOB_URL)
 job_api = ast.literal_eval(job_api)
