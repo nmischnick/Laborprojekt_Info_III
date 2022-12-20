492319cc6bd921117d6c969f67d022211a4b3b00
MarcelLuzi <103583346+MarcelLuzi@users.noreply.github.com>
2022-12-20 12:17:58 +0100

    Create test_datenbank.py

---
+++ Datenbank/test_datenbank.py
@@ -0,0 +1,68 @@
+from unittest.mock import MagicMock
+import random
+import string
+
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def random_int(a=0, b=200):
+    return random.randint(a, b)
+
+def random_float(a=0, b=200):
+    return random.uniform(a, b)
+
+display = id_generator()
+date = random_int()
+
+
+def sample_data_printer():
+    state = id_generator()
+    temp_tool_i = random_float()
+    temp_tool_s = random_float()
+    temp_bed_i = random_float()
+    temp_bed_s = random_float()
+
+    sample_data = {"state":state, "temp_tool_i": temp_tool_i, "temp_tool_s": temp_tool_s, "temp_bed_i": temp_bed_i, "temp_bed_s": temp_bed_s}
+
+    return sample_data
+
+def sample_data_job():
+    averagePrintTime = random_float(0, 50)
+    volume = random_float(0, 50)
+
+    sample_data = {"averagePrintTime":averagePrintTime, "volume": volume, "display": display, "date": date}
+
+    return sample_data
+
+#
+
+def sample_data_files():
+
+    files = []
+
+    for i in range(2):
+        hash = id_generator(25)
+        #display = id_generator()
+        download = id_generator()
+        free = random_int(10000000, 100000000)
+
+        sample_data = {"hash": hash, "date": date, "display": display, "download": download, "free": free}
+
+        files.append(sample_data)
+
+
+    return files
+
+
+def test_can_insert_data():
+    octoprint = MagicMock()
+    octoprint.get_printer_data.return_value = sample_data_printer()
+    octoprint.get_files_data.return_value = sample_data_files()
+    octoprint.get_jobs_data.return_value = sample_data_job()
+    printer = octoprint.get_printer_data()
+    files = octoprint.get_files_data()
+    jobs = octoprint.get_jobs_data()
+    #to_database_all(printer, files, jobs)
+
+    assert True
