8b71317b71766b5999d9c10b617f949d0abd0739
luis <luis.kl@hotmail.de>
2022-12-21 15:52:01 +0100

    zusammenspiel test

--- zusammen.py
+++ zusammen.py
@@ -1,11 +1,12 @@
-from Datenbank.datenbank import create_database, to_database_files
+from Datenbank.datenbank import create_database, to_database_all
 from Octoprint.json_filter import files_api_f, printer_api_f, job_api_f
 
 
 create_database()
 
 files = files_api_f()
-#printer = printer_api_f()
-#jobs = job_api_f()
+printer = printer_api_f()
+jobs = job_api_f()
 
-to_database_files(files)
+#to_database_files(files)
+to_database_all(files, jobs, printer)
