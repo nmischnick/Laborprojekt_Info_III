1977edf36d805cae1895d926c8092976d7e3759a
luis <luis.kl@hotmail.de>
2022-12-22 11:10:14 +0100

    fixes

--- Octoprint/json_filter.py
+++ Octoprint/json_filter.py
@@ -5,7 +5,7 @@ Datum: 21.12.2022
 """
 import ast
 import json
-import octoprint
+from Octoprint.octoprint import get_json
 
 PRINTER_URL = "http://141.41.42.192/api/printer?apikey=A7DBE849344A42A0B22C50CA22EC6210"
 JOB_URL = "http://141.41.42.192/api/job?apikey=A7DBE849344A42A0B22C50CA22EC6210"
@@ -13,20 +13,22 @@ FILES_URL = "http://141.41.42.192/api/files?apikey=A7DBE849344A42A0B22C50CA22EC6
 
 def printer_api_f():
     "Die Funktion ruft den String vom Server bearbeitet den Json_printer_api_string. Dabei werden die False Werte rausgelöscht."
-    printer_api = octoprint.get_json(PRINTER_URL)  # json str wird aufgerufen
+    printer_api = get_json(PRINTER_URL)  # json str wird aufgerufen
     printer_api = ast.literal_eval(printer_api)  # wertet die json Funktion mit trees richtig aus
     printer_api = json.dumps(printer_api)  # konvertiert den ausgewerteten Wert in den passenden JSON
     json_info = json.loads(printer_api)            #json wird gelesen
     state = json_info["state"]                  # json baum wird definiert
     flags = state['flags']                      #json unterbaum wird definiert
-    state = {}
+    state = ""
     for key, value in dict(flags).items():
         if value is False:                      # wenn key = false wird entfernt
             del flags[key]
-        else:                                   #wenn key != false wird er in die dict hinzugefügt
-            name = key
-            value = flags[key]
-            state[name] = value
+        elif key in ["error", "printing", "paused", "ready"]:                                   #wenn key != false wird er in die dict hinzugefügt
+
+            state = key
+
+        else:
+            state = "ready"
 
     temperatur = json_info['temperature']       #daten definieren mit path
     temp_bed_i = temperatur['bed']['actual']
@@ -38,7 +40,7 @@ def printer_api_f():
 
 def job_api_f():
     "Die Funktion ruft den String vom Server und bearbeitet den Json_job_api_string. Die wichtigen Werte werden im neuen Dict gespeichert."
-    job_api = octoprint.get_json(JOB_URL)
+    job_api = get_json(JOB_URL)
     job_api = ast.literal_eval(job_api)
     job_api = json.dumps(job_api)
     json_info = json.loads(job_api)                            #json wird gelesen
@@ -51,7 +53,7 @@ def job_api_f():
 
 def files_api_f():
     "Die Funktion ruft den String vom Server bearbeitet den Json_files_api_string. Da der String aus mehreren Bäumen besteht, werden die nacheinander bearbeitet."
-    files_api = octoprint.get_json(FILES_URL)
+    files_api = get_json(FILES_URL)
     files_api = ast.literal_eval(files_api)
     files_api = json.dumps(files_api)
     json_info = json.loads(files_api)    #json wird gelesen
@@ -59,6 +61,10 @@ def files_api_f():
     liste =[]
     for i in range(0,len(files)):
         file = files[i]                     #der n tree des Files wird ausgewählt
+        try:
+            date = file['date']
+        except:
+            date = None
         try:
             display = file['display']           #daten werden definiert mit path
         except:
@@ -79,7 +85,7 @@ def files_api_f():
             success = json_info['prints']['success']
         except:
             success = None
-        files_api_data = {'hash':hash_data, 'display':display,'download':download,'free':free,'success':success} #daten in eigene dict
+        files_api_data = {'date': date, 'hash':hash_data, 'display':display,'download':download,'free':free,'success':success} #daten in eigene dict
         liste.append(files_api_data)
     return liste
 
---
+++ zusammen.py
@@ -0,0 +1,18 @@
+from Datenbank.datenbank import create_database, to_database_all
+from Datenbank.datenbank import storage_progress, count_states, temp_progress
+from Octoprint.json_filter import files_api_f, printer_api_f, job_api_f
+import threading
+import datetime
+
+create_database()
+
+def dostuff():
+    files = files_api_f()
+    printer = printer_api_f()
+    jobs = job_api_f()
+
+    threading.Timer(10.0, dostuff).start()
+    to_database_all(files, jobs, printer)
+
+
+dostuff()
